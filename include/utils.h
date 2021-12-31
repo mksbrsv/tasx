c5038847b14f6eeb9e151d01e1fea80bdff54af8
Max Borisov <arryowtf@gmail.com>
2021-12-31 16:05:04 +0300

    fix help option

--- include/utils.h
+++ include/utils.h
@@ -10,15 +10,16 @@ const std::string TASX_FILE =
     HOME + "/code/cpp/tasx/todo.json";  // TODO: change folder to config folder
 
 const std::string HELP =
-    "Usage:\n tasx [<options>...]\n"
+    "Usage:\n tasx [<options>...]\n\n"
     "Options:\n"
+    "none           Show stats\n"
     "--help, -h     Help menu\n"
     "--list, -l     Display list of tasks\n"
     "--done, -d     Mark task done\n"
     "--new, -n      Create new task\n"
-    "--priority, -n Set priority [id] [priority{1-4}]"
+    "--priority, -n Set priority [id] [priority{1-4}]\n"
     "--remove, -r   Remove task from list\n"
-    "--clear, -c    Clear list\n"
+    "--clear, -c    Clear list\n\n"
     "Priority:\n"
     "1. Low - white color\n"
     "2. Medium - yellow color\n"
